59bf1af58b0d9301015da0ddf34e392f8b617802
Josesan94 <josesantillan2@hotmail.com>
2024-04-23 18:47:01 -0300

    create PokemonCard component

---
+++ frontend/src/components/PokemonCard.tsx
@@ -0,0 +1,50 @@
+import { Card, CardContent, Typography, CardMedia, Chip, LinearProgress, Box } from '@mui/material';
+import { usePokemonDetails } from '../hooks/usePokemonDetails'; // Asegúrate de ajustar la ruta de importación según tu estructura de directorios
+
+interface PokemonCardInterface {
+    pokemonUrl: string;
+}
+
+const PokemonCard = ({ pokemonUrl }:PokemonCardInterface) => {
+  const { pokemon: details, isLoading, isError } = usePokemonDetails(pokemonUrl);
+
+  if (isError) return <div>Error loading details.</div>;
+  if (isLoading || !details) return <div>Loading details...</div>;
+
+  const normalize = (value:any) => value * 100 / 255; // Normalizo los valores para la barra de progreso.
+
+
+  return (
+    <Card className="transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 bg-white shadow-lg rounded-lg overflow-hidden">
+      <CardMedia
+        component="img"
+        image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${details.id}.png`}
+        alt={details.name}
+        className="w-full h-50 object-fit"
+
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h4" component="div" className="text-center capitalize">
+          {details.name}
+        </Typography>
+        <div className="flex justify-center flex-wrap gap-2 mb-4">
+          {details.types.map((type:any, index:any) => (
+            <Chip key={index} label={type.type.name} className="capitalize" color="primary" />
+          ))}
+        </div>
+        <div className="space-y-2">
+          {details.stats.map((stat:any, index:any) => (
+            <Box key={index}>
+                <Typography variant="body1" color="text.secondary" className="font-medium">
+                  {stat.stat.name.replace('-', ' ')}: {stat.base_stat}
+                </Typography>
+                <LinearProgress variant="determinate" value={normalize(stat.base_stat)} />
+            </Box>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PokemonCard;
