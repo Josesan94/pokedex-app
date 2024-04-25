bf37d4ea14e6db8447a582fcda491d2bfec69ad7
Josesan94 <josesantillan2@hotmail.com>
2024-04-24 22:50:49 -0300

    create controller and services for type API

--- backend/src/controllers/pokemon.controller.ts
+++ backend/src/controllers/pokemon.controller.ts
@@ -27,12 +27,47 @@ export const getPokemonDetails = async (req: Request, res: Response) => {
     }
 }
 
-export const searchPokemons = async (req: Request, res: Response) => {
-    const { query } = req.query;
+export const getPokemonsByName = async (req: Request, res: Response) => {
+    const { name } = req.params;
     try {
-        const pokemons = await pokemonService.searchPokemons(query as string);
+        const pokemons = await pokemonService.searchPokemonByName(name);
         res.json(pokemons);
     } catch (error:any) {
         res.status(404).send(error.message);
+    }
+};
+
+export const getPokemonsByType = async (req:Request, res:Response) => {
+    const {type} = req.params;
+    try {
+        const results = await pokemonService.searchPokemonByType(type);
+        res.json(results)
+    } catch(error:any) {
+        res.status(500).send(error.message);
+    }
+}
+
+// Modificar uno de los controladores existentes para aceptar ambos parámetros
+export const getPokemons = async (req: Request, res: Response) => {
+    const { name, type } = req.query;
+    try {
+        if (type) {
+            const typeResults = await pokemonService.searchPokemonByType(type.toString());
+            if (name) {
+                const filteredResults = typeResults.filter((pokemon:any) => pokemon.name.includes(name));
+                res.json(filteredResults);
+                return;
+            }
+            res.json(typeResults);
+            return;
+        } else if (name) {
+            const nameResults = await pokemonService.searchPokemonByName(name.toString());
+            res.json(nameResults);
+            return;
+        }
+        res.json([]);
+    } catch (error:any) {
+        console.error('Error fetching Pokemons:', error);
+        res.status(500).send(error.message);
     }
 };
--- backend/src/routes/pokemon.routes.ts
+++ backend/src/routes/pokemon.routes.ts
@@ -5,7 +5,11 @@ const router = Router();
 
 router.get('/pokemons', pokemonController.listPokemons);
 router.get('/pokemon/:pokemonId', pokemonController.getPokemonDetails);
-router.get('/search', pokemonController.searchPokemons);
+router.get('/search_type/:name', pokemonController.getPokemonsByName);
+router.get('/search_type/:type', pokemonController.getPokemonsByType);
+// En tus rutas (routes.js o similar)
+router.get('/search', pokemonController.getPokemons);
+
 
 
 export default router;
