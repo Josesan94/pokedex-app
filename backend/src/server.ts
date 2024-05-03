c862de1cd6d4b7a3f6425547bc80caeeb6667199
Josesan94 <josesantillan2@hotmail.com>
2024-05-03 14:54:10 -0300

    implement swagger setup into backend app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,15 @@
 import app from './app';
+import dotenv from 'dotenv';
+const path = require('path');
+import { setupSwagger } from '../config/swagger';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    setupSwagger(app)
 });
