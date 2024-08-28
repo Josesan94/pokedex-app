449295a2e9381ddd0c24fc8e6451b5a532aa9007
Josesan94 <josesantillan2@hotmail.com>
2024-08-28 13:05:53 -0300

    add config to accept images

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,7 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock para estilos
+    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/mocks/fileMock.ts' // Mock para archivos de imagen y otros tipos de archivo
   },
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', {
